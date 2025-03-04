c4e9b83ae1592a33a714a263d1375144e82b5498
NedLee1005 <136004827+NedLee1005@users.noreply.github.com>
2025-03-04 17:00:02 +0800

    Update Lab01 Good

--- Lab01/Lab01.py
+++ Lab01/Lab01.py
@@ -1,59 +1,71 @@
 from gpiozero import LED
-from requests import get ##import requests 中的get方法
-import json ## 匯入json Library
+from requests import get
+import json
 import time
-Firebase_URL = 'https://raspberry-pi-01-27424.firebaseio.com/.json'
-# https://lab01-ee469-default-rtdb.firebaseio.com/
 
-## 新增一個存放剛剛的firebase 的連結
+Firebase_URL = 'https://lab01-ee469-default-rtdb.firebaseio.com/.json'
+
 led17 = LED(17)
-led22 = LED(22) ##設定led變數
+led22 = LED(22)
 led27 = LED(27)
 led10 = LED(5)
 led09 = LED(6)
-led11 = LED(13) 
+led11 = LED(13)
+
 led17.off()
-led22.off() ##在開始執行程式前先將LED全部關閉
+led22.off()
 led27.off()
 led10.off()
 led09.off()
 led11.off()
+
 while True:
-    LedData = get(Firebase_URL).json()['LED']
-## 宣告ledData，並讓它用get 方式去讀取Firebase的網址，並轉成json 格式，
-取得資料庫裡led 欄位底下的資料。
-led17onoff = LedData['LED_R'] ## 讀取led17的狀態
-led22onoff = LedData['LED_G'] ## 讀取led22的狀態
-led27onoff = LedData['LED_B'] ## 讀取led27的狀態
-led09onoff = LedData['LED-2-R']
-led10onoff = LedData['LED-2-G']
-led11onoff = LedData['LED-2-B']
-Flash=LedData['Flashing']
-led17.value = led17onoff
-led22.value = led22onoff
-led27.value = led27onoff
-led09.value = led09onoff
-led10.value = led10onoff
-led11.value = led11onoff
-if flash == 1:
-    count=0
-    for count in range(0,8):
-        if count%2==0:
-            led17.value=0
-            led10.value=0
-        else:
-            led17.value=1
-            led10.value=1
-        if count<4:
-            led22.value=0
-            led09.value=0
-        else:
-            led22.value=1
-	    led09.value=1
-	    if count==0 or count==1 or count==4 or count==5:
-	        led27.value=0
-		led11.value=0
-            else:
-	        led27.value=1
-		led11.value=1
-	    time.sleep(.300)
+    response = get(Firebase_URL)  # Get the response from Firebase
+    print(response.json())  # Debugging: print the full response to inspect the structure
+    
+    LedData = response.json().get('LED', {})  # Get the 'LED' data, or empty dictionary if not found
+    
+    if LedData:  # If the 'LED' data is found, process it
+        led17onoff = LedData.get('LED_R', 0)
+        led22onoff = LedData.get('LED_G', 0)
+        led27onoff = LedData.get('LED_B', 0)
+        led09onoff = LedData.get('LED-2-R', 0)
+        led10onoff = LedData.get('LED-2-G', 0)
+        led11onoff = LedData.get('LED-2-B', 0)
+        Flash = LedData.get('Flashing', 0)
+
+        led17.value = led17onoff
+        led22.value = led22onoff
+        led27.value = led27onoff
+        led09.value = led09onoff
+        led10.value = led10onoff
+        led11.value = led11onoff
+
+        if Flash == 1:
+            count = 0
+            for count in range(0, 8):
+                if count % 2 == 0:
+                    led17.value = 0
+                    led10.value = 0
+                else:
+                    led17.value = 1
+                    led10.value = 1
+
+                if count < 4:
+                    led22.value = 0
+                    led09.value = 0
+                else:
+                    led22.value = 1
+                    led09.value = 1
+
+                if count == 0 or count == 1 or count == 4 or count == 5:
+                    led27.value = 0
+                    led11.value = 0
+                else:
+                    led27.value = 1
+                    led11.value = 1
+
+                time.sleep(0.300)
+    else:
+        print("No LED data found in Firebase response")
+        time.sleep(1)
